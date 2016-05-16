601ba8a66d5f789cc328169bbdda5e6a7311abb6
Hal Blackburn <hwtb2@cam.ac.uk>
2016-05-16 11:06:49 +0100

    Allow overriding default config using script tag
    This will allow the dev/staging/live viewers to use different configs
    with the same underlying build.

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -8,6 +8,24 @@ import '../stylesheets/player.css';
 
 window.__cudlEmbeddedVersion = VERSION;
 
+function getConfig() {
+    // CONFIG global is set via the webpack define plugin
+    let config = CONFIG;
+
+    // Allow overriding the config with a script tag w/ id config.
+    let configElement = $('script#config[type="application/json"]');
+    if(configElement.length) {
+        try {
+            Object.assign(config, JSON.parse(configElement.text()));
+        }
+        catch(e) {
+            console.error('Unable to parse config: ' + configElement.text(), e);
+        }
+    }
+
+    return config;
+}
+
 $(function() {
     // In dev mode we're using the webpack dev server to provide the HTML
     // which loads our js bundle, so we need to render the HTML outself.
@@ -25,6 +43,9 @@ $(function() {
             .prepend('<meta name="viewport" content="initial-scale=1">');
     }
 
+    let config = getConfig();
+    console.debug('Using config: ', config);
+
     // player.js needs to be loaded after the DOM is loaded
-    require('./player.js').initFromConfig(CONFIG);
+    require('./player.js').initFromConfig(config);
 });
